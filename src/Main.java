d1514fa12f927c10fe87639fc95869921e14032b
chaostocosmos <kml1763@gmail.com>
2023-02-13 08:55:42 +0900

    test commit : git commit 연계 next algo sol : 문자열 찾기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+// 첫 줄에 문자열이 주어지고, 두번쨰 줄에 문자가 주어진다.
+// 문자열은 영어 알파벳으로만 구성되어 있습니다.
+
+// 첫 줄에 해당 문자의 갯수를 출력한다.
+
+import java.util.Scanner;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public int solution(String str, char t){
+
+        int answer = 0;
+
+        return answer;
+
+
+    }
+
+    public static void main(String[] args){
+
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+
+
+
+        System.out.println();
+
     }
 }
+
