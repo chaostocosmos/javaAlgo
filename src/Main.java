5ddba5d600ba47107646f3bfde394049e5f476a3
chaostocosmos <kml1763@gmail.com>
2023-02-13 09:26:01 +0900

    문자열 찾기

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,27 @@
-// 첫 줄에 문자열이 주어지고, 두번쨰 줄에 문자가 주어진다.
-// 문자열은 영어 알파벳으로만 구성되어 있습니다.
 
-// 첫 줄에 해당 문자의 갯수를 출력한다.
 
 import java.util.Scanner;
 
 
 public class Main {
 
-    public int solution(String str, char t){
-
+    public int solution1(String str, char t){
+        // 첫 줄에 문자열이 주어지고, 두번쨰 줄에 문자가 주어진다.
+        // 문자열은 영어 알파벳으로만 구성되어 있습니다.
+        // 첫 줄에 해당 문자의 갯수를 출력한다.
         int answer = 0;
+
+        str = str.toUpperCase(); // 전체 대문자 변경
+        t = Character.toUpperCase(t);
+
+        //System.out.println(str);
+        //System.out.println(t);
+
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i)==t){
+                answer++;
+            }
+        }
 
         return answer;
 
@@ -24,9 +35,7 @@ public class Main {
         String str = kb.next();
         char c = kb.next().charAt(0);
 
-
-
-        System.out.println();
+        System.out.println(T.solution1(str,c));
 
     }
 }
