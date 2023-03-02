b9654ce353bc01bb776a357e4bf81502b6a21104
JINWON KIM <105859609+chaostocosmos@users.noreply.github.com>
2023-03-02 11:15:23 +0900

    Update Main.java

--- JavaAlgo2/src/Main.java
+++ JavaAlgo2/src/Main.java
@@ -7,8 +7,8 @@ N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램
 
 
  */
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -24,7 +24,33 @@ public class Main {
 //            System.out.println(x);
 //        }
         String str = sc.next();
-        System.out.println(T.solution2(str));
+        System.out.println(T.solution3(str));
+    }
+
+    public String solution3(String str){
+        String answer = "";
+        List<Character> as = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        char[] ca = str.toCharArray();
+        int lt = 0 , rt = ca.length-1;
+        //char array를 순회하면서,
+        //그 값이 있는지 없는지 찾기
+        //그래서 해당 값이 있으면 첫자를 남기고 다음 값은 제거
+
+        for(lt=0;lt<=rt;lt++){
+
+            if(!as.contains(ca[lt])){
+                as.add(ca[lt]);
+                sb.append(ca[lt]);
+            }
+
+        }
+
+        answer = String.valueOf(sb);
+
+
+        return answer;
+
     }
 
     public String solution2(String str){
