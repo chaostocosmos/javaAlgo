c2cfde18d2e670f419961650d2c715c3ae70c564
chaostocosmos <kml1763@gmail.com>
2023-03-03 09:40:43 +0900

    앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 합니다.
    문자열이 입력되면 해당 문자열이 회문 문자열이면 "YES", 회문 문자열이 아니면 “NO"를 출력하는 프로그램을 작성하세요.
    
    단 회문을 검사할 때 대소문자를 구분하지 않습니다.

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,45 @@ import java.util.List;
 import java.util.Scanner;
 
 
+
+//회문 문자열
+/*
+앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 합니다.
+
+문자열이 입력되면 해당 문자열이 회문 문자열이면 "YES", 회문 문자열이 아니면 “NO"를 출력하는 프로그램을 작성하세요.
+
+단 회문을 검사할 때 대소문자를 구분하지 않습니다.
+ */
+
+
 public class Main {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Main T = new Main();
-        Scanner in=new Scanner(System.in);
-        String input1 = in.nextLine();
-        //int input2 = in.nextInt();
-        System.out.println(T.solution3(input1));
-        return ;
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(T.solution6(str));
     }
+
+    private String solution6(String str) {
+        String answer = "";
+        str = str.toUpperCase();
+        StringBuilder sb = new StringBuilder();
+        sb.append(str);
+        String restring = sb.reverse().toString();
+        //System.out.println(restring);
+        if(restring.equals(str)){
+            return "YES";
+        }
+        else {
+            return "NO";
+        }
+
+
+
+    }
+
 
     public String solution3(String str){
 
