9bd35cd3d775f3c77b44ce440da6fd3354a1ea03
chaostocosmos <kml1763@gmail.com>
2023-03-03 22:37:51 +0900

    3번 가장 긴 단어 출력하기 1. split을 통한 방법 . 2. indexof와 substring으로 출력하기

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,100 @@ import java.util.Scanner;
 단 회문을 검사할 때 대소문자를 구분하지 않습니다.
  */
 
+//유효한 펠린드롬
+/*
+앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 팰린드롬이라고 합니다.
+
+문자열이 입력되면 해당 문자열이 팰린드롬이면 "YES", 아니면 “NO"를 출력하는 프로그램을 작성하세요.
+
+단 회문을 검사할 때 알파벳만 가지고 회문을 검사하며, 대소문자를 구분하지 않습니다.
+
+알파벳 이외의 문자들의 무시합니다.
+
+found7, time: study; Yduts; emit, 7Dnuof
+foundtimestudy
+ydutsemitdnuof
+// https://inpa.tistory.com/entry/JAVA-%E2%98%95-%EC%A0%95%EA%B7%9C%EC%8B%9DRegular-Expression-%EC%82%AC%EC%9A%A9%EB%B2%95-%EC%A0%95%EB%A6%AC
+자바 정규식
+
+1. 문자열을 알파벳으로 모두 변경한다. ( 정규식을 이용 )
+2. 정규식으로 변경된 알파벳이 회문문자인지 판별한다.
+
+YES
+
+.	임의의 문자 1개를 의미
+^	시작을 의미한다
+[] 괄호 안에 있다면 일치하지 않는 부정의 의미로로 쓰인다	^a : a로 시작하는 단어
+[^a] : a가 아닌 철자인 문자 1개
+$	$앞의 문자열로 문자가 끝나는지를 의미한다	a$ : a로 끝나는 단어
+[]	[] 괄호 안의 문자가 있는지를 확인한다	[ab][cd] : a,b중 한 문자와 c,d중 한 문자
+→ ac ad bc bd
+[^]	[] 대괄호 안에 ^ 문자가 있으면, 제외를 뜻함
+
+- 대괄호 안에 ^ 가 쓰이면 제외의 뜻
+- 대괄호 밖에 ^ 가 쓰이면 시작점의 뜻	[^a-z] : 알파벳 소문자 a부터 z까지를 제외한 모든 문자
+-	사이의 문자 혹은 숫자를 의미한다	[a-z] : 알파벳 소문자 a부터 z까지 하나
+[a-z0-9] : 알파벳 소문자 전체,0~9 중 한 문자
+|	또는	[a|b] : a 혹은 b
+()	그룹	01(0|1) : 01뒤에 0 또는 1이 들어간다
+→ 010(o), 011(o), 012(x)
+{}	개수	a{3}b : a가 3번 온 후 b가 온다
+→ aab(x), aaab(o), aaaab(o)
+\b	공백, 탭, ",", "/" 등을 의미한다	apple\b : apple뒤에 공백 탭등이 있다
+→ apple juice (o), apple.com (x)
+\B	\b의 부정
+공백 탭등이 아닌 문자인 경우 매치한다	apple\B
+→ apple.com (o)
+\d	0~9 사이의 숫자
+[0-9]와 동일
+\D	\d의 부정
+숫자가 아닌 어떤 문자, [^0-9]와 동일
+\s	공백, 탭
+\S	공백, 탭이 아닌 문자
+\w	알파벳 대소문자+숫자+"_"
+
+[a-zA-Z_0-9]와 동일
+\W	\w의 부정
+
+[^a-zA-Z_0-9]
+
+
+정규식 수량 기호
+기호	설명	예제
+?	앞의 표현식이 없거나 or 최대 한개만	a1? : 1이 최대 한개만 있거나 없을수도 있다
+→ a(o), a1(o), a11(x), a111(x)
+*	앞의 표현식이 없거나 or 있거나 (여러개)	a1* : 1이 있을수도 없을수도 있다
+→ a(o), a1(o), a11(o), a111(o)
++	앞의 표현식이 1개 이상 or 여러개	a1* : 1이 1개 이상있다
+→ a(x), a1(o), a11(o), a111(o)
+{n}	딱 n개 있다	a{3} : a가 딱 3개 있다
+→ aa(x), aaa(o), aaaa(x), aaaaaaa(x)
+{n, m}	n개 이상 m개 이하	a{3,5} : a가 3개 이상 5개 이하 있다
+→ aa(x), aaa(o), aaaa(o), aaaaaaa(x)
+{n,}	n개 이상	a{3,} : a가 3개 이상 있다
+→ aa(x), aaa(o), aaaa(o), aaaaaaa(o)
+
+
+정규식 그룹 캡쳐 기호
+기호	설명
+()	그룹 및 캡쳐
+(?:)	찾지만 그룹에 포함 안됨
+(?=)	=앞 문자를 기준으로 전방 탐색
+(?<=)	=뒤 문자를 기준으로 후방 탐색
+
+
+자주 사용되는 정규식 샘플
+정규 표현식	설명
+^[0-9]*$	숫자
+^[a-zA-Z]*$	영문자
+^[가-힣]*$	한글
+\\w+@\\w+\\.\\w+(\\.\\w+)?	E-Mail
+^\d{2,3}-\d{3,4}-\d{4}$	전화번호
+^01(?:0|1|[6-9])-(?:\d{3}|\d{4})-\d{4}$	휴대전화번호
+\d{6} \- [1-4]\d{6}	주민등록번호
+^\d{3}-\d{2}$	우편번호
+
+ */
 
 public class Main {
 
@@ -21,9 +115,31 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        System.out.println(T.solution6(str));
+        String str = sc.nextLine();
+        System.out.println(T.solution7(str));
     }
+
+
+    //유효한 펠랜드롬
+    private String solution7(String str){
+
+        String answer = "YES";
+        String tmp = "";
+        str = str.toUpperCase().replaceAll("[^A-Z]","");
+        //tmp = str.replaceAll("^[a-zA-Z]*$"," ");
+        StringBuilder sb = new StringBuilder();
+        sb.append(str);
+        System.out.println(str);
+        String reverstring = sb.reverse().toString();
+        if(!reverstring.equalsIgnoreCase(str)){
+            return "NO";
+        }
+
+        return answer;
+
+
+    }
+
 
     private String solution6(String str) {
         String answer = "";
