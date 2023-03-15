b2c4ac1915e432bd5b9d4e618bf8540f4d920be4
chaostocosmos <kml1763@gmail.com>
2023-03-15 22:43:36 +0900

    Chapter2 가위바위보

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,29 @@ import java.util.Scanner;
 8
 130 135 148 140 145 150 150 153
 
+가위 바위 보
+A, B 두 사람이 가위바위보 게임을 합니다. 총 N번의 게임을 하여 A가 이기면 A를 출력하고, B가 이기면 B를 출력합니다. 비길 경우에는 D를 출력합니다.
+가위, 바위, 보의 정보는 1:가위, 2:바위, 3:보로 정하겠습니다.
+예를 들어 5이면
+c :1 2 3 4 5
+A :2 3 3 1 3
+B :1 1 2 2 3
+A B A B D
+두 사람의 각 회의 가위, 바위, 보 정보가 주어지면 각 회를 누가 이겼는지 출력하는 프로그램을 작성하세요.
+
+입력
+첫 번째 줄에 게임 횟수인 자연수 N(1<=N<=100)이 주어집니다.
+두 번째 줄에는 A가 낸 가위, 바위, 보 정보가 N개 주어집니다.
+세 번째 줄에는 B가 낸 가위, 바위, 보 정보가 N개 주어집니다.
+각 줄에 각 회의 승자를 출력합니다. 비겼을 경우는 D를 출력합니다.
+
+5
+2 3 3 1 3
+1 1 2 2 3
+3
+1 1 1
+1 1 1
+
  */
 public class Main {
     public static void main(String[] args) {
@@ -19,16 +42,50 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
+        int[] arr2 = new int[n];
         for(int i=0; i<n; i++){
             arr[i]=sc.nextInt();
         }
-        System.out.println(T.solution2(n,arr));
+        for(int j=0; j<n; j++){
+            arr2[j]=sc.nextInt();
+        }
+
+        for(String x : T.solution3(n,arr,arr2)){
+            System.out.println(x);
+        }
+
+        //System.out.println(T.solution2(n,arr));
         // 인자로 넘겨주는데서 다 처리하면 왜 함수로 넘기냐 여기서 그냥 다처리하지 ㅡㅡ
     }
 
+    private String[] solution3(int n, int[] arr, int[] arr2){
+
+        /*
+        썜 답안
+
+         */
+        String[] answer = new String[n];
+        for(int i=0; i<n; i++){
+            if(arr[i]==arr2[i]){
+                answer[i]="D";
+            }
+            else if (arr[i]==1&&arr2[i]==3 || arr[i]==2&&arr2[i]==1 || arr[i]==3&&arr2[i]==2){
+                answer[i]="A";
+
+            }
+            else {
+                answer[i]="B";
+            }
+
+        }
+
+
+        return answer;
+
+    }
 
     private int solution2(int n , int[] arr){
-
+        
         int answer = 0;
         int maxcnt = 0;
 
