d112576db8cb868d37bc8da8171f6c975a5b1027
chaostocosmos <kml1763@gmail.com>
2023-03-18 18:46:59 +0900

    Chapter2 OX 문제 count를 잘 활용하자 함수 활용하기

--- src/Main.java
+++ src/Main.java
@@ -80,6 +80,24 @@ N개의 자연수가 입력되면 각 자연수를 뒤집은 후 그 뒤집은 �
 2. 뒤집힌 수가 소수인지 판별해야함
 
 
+OX 문제는 맞거나 틀린 두 경우의 답을 가지는 문제를 말한다.
+여러 개의 OX 문제로 만들어진 시험에서 연속적으로 답을 맞히는 경우에는 가산점을 주기 위해서 다음과 같이 점수 계산을 하기로 하였다.
+1번 문제가 맞는 경우에는 1점으로 계산한다. 앞의 문제에 대해서는 답을 틀리다가 답이 맞는 처음 문제는 1점으로 계산한다.
+또한, 연속으로 문제의 답이 맞는 경우에서 두 번째 문제는 2점, 세 번째 문제는 3점, ..., K번째 문제는 K점으로 계산한다. 틀린 문제는 0점으로 계산한다.
+예를 들어, 아래와 같이 10 개의 OX 문제에서 답이 맞은 문제의 경우에는 1로 표시하고, 틀린 경우에는 0으로 표시하였을 때,
+
+점수 계산은 아래 표와 같이 계산되어, 총 점수는 1+1+2+3+1+2=10 점이다.
+1 0 1 1 1 0 0 1 1 0
+
+1 0 1 2 3 0 0 1 2 0
+시험문제의 채점 결과가 주어졌을 때, 총 점수를 계산하는 프로그램을 작성하시오.
+
+첫째 줄에 문제의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에는 N개 문제의 채점 결과를 나타내는 0 혹은 1이 빈 칸을 사이에 두고 주어진다.
+0은 문제의 답이 틀린 경우이고, 1은 문제의 답이 맞는 경우이다.
+
+10
+1 0 1 1 1 0 0 1 1 0
+
  */
 public class Main {
     public static void main(String[] args) {
@@ -95,16 +113,38 @@ public class Main {
         for(int i=0; i<n; i++){
             arr[i]=sc.nextInt();
         }
+        System.out.println(T.solution8(n,arr));
 //        for(int j=0; j<n; j++){
 //            arr2[j]=sc.nextInt();
 //        }
 //
-        for(int x : T.solution6(n,arr)){
-            System.out.print(x + " ");
-        }
+
 
         //System.out.println(T.solution2(n,arr));
         // 인자로 넘겨주는데서 다 처리하면 왜 함수로 넘기냐 여기서 그냥 다처리하지 ㅡㅡ
+    }
+
+    private int solution8(int n, int[] arr) {
+        int answer = 0;
+        int count = 0;
+        int[] score = new int[n];
+        for(int i=0; i<n;i++){
+            if(arr[i]==1){
+                score[i] = 1 + count;
+                count++;
+            }
+            else {
+                score[i] = 0;
+                count = 0;
+            }
+
+        }
+
+        for(int x : score){
+            answer += x;
+        }
+
+        return answer;
     }
 //1. 수를 뒤집어야 하고
 //1의 자리일때는 나머지를 구하고 10의 자리일땐 나눈다 100의 자리일떄도 나눈다
