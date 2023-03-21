4cd7a059cb44a767642b9c26c1fdcb6ab306ba20
chaostocosmos <kml1763@gmail.com>
2023-03-21 21:53:10 +0900

    Chapter2 격자판 최대합 ( 강의 코드 포함 )

--- src/Main.java
+++ src/Main.java
@@ -133,7 +133,29 @@ N*N의 격자판이 주어지면 각 행의 합, 각 열의 합, 두 대각선�
 19 27 29 37 27
 19 13 30 13 19
 
+지도 정보가 N*N 격자판에 주어집니다. 각 격자에는 그 지역의 높이가 쓰여있습니다.
+각 격자판의 숫자 중 자신의 상하좌우 숫자보다 큰 숫자는 봉우리 지역입니다. 봉우리 지역이 몇 개 있는 지 알아내는 프로그램을 작성하세요.
+격자의 가장자리는 0으로 초기화 되었다고 가정한다.
+만약 N=5 이고, 격자판의 숫자가 다음과 같다면 봉우리의 개수는 10개입니다.
 
+첫 줄에 자연수 N이 주어진다.(2<=N<=50)
+두 번째 줄부터 N줄에 걸쳐 각 줄에 N개의 자연수가 주어진다. 각 자연수는 100을 넘지 않는다.
+봉우리의 개수를 출력하세요.
+
+5
+5 3 7 2 3
+3 7 1 6 1
+7 2 5 3 4
+4 3 6 4 1
+8 7 3 5 2
+
+6
+1 2 3 4 5 6
+1 2 3 4 5 6
+1 2 3 4 5 6
+1 2 3 4 5 6
+1 2 3 4 5 6
+1 2 3 4 5 6
 
  */
 public class Main {
@@ -142,17 +164,67 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int[][] arr = new int[n][n];
-        for(int i=0; i<n; i++){
-            for (int j=0; j<n; j++){
+        int[][] arr = new int[n+2][n+2];
+
+        for(int i=1; i<n+1; i++){
+            for (int j=1; j<n+1; j++){
                 arr[i][j] = sc.nextInt();
+
             }
         }
 
-        System.out.println(T.solution10(n,arr));
+        System.out.println(T.solution11(n,arr));
 
 
 
+    }
+    /*
+    격자 안 봉우리
+    Class Main
+    int [] dx = {-1,0,1,0};
+    int [] dy = {0,1,0,-1};
+    public int solution(int n , int[][] arr){
+        int answer=0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                boolean flag = true;
+                for(int k = 0 ; k<4; k++){
+                    int nx = i+dx[k];
+                    int ny = i+dy[k];
+                    if( nx>=0 && nx<n && ny>=0 && ny<n && arr[nx][ny]>=arr[i][j]){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) answer++;
+            }
+        }
+        return answer;
+    }
+    void main
+    int n = kb.nextInt();
+    int[][] arr = new int[n][n];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            arr[i][j] = kb.nextInt();
+        }
+    System.out.print(T.solution11(n,arr));
+     */
+    private int solution11(int n, int[][] arr) {
+        int answer = 0;
+
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(arr[i][j]>arr[i-1][j]&&arr[i][j]>arr[i+1][j]&&arr[i][j]>arr[i][j-1]&&arr[i][j]>arr[i][j+1]){
+                    answer++;
+                }
+            }
+        }
+
+
+        return answer;
     }
 
     /*
