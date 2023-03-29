82b2d15b5b6503bb0bda5495b451868ce5c7307a
chaostocosmos <kml1763@gmail.com>
2023-03-29 23:22:06 +0900

    Chapter3 two pointers 공통인자 찾기

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 
 
@@ -23,10 +24,39 @@ public class Main {
         }
 
 
-        for(int x : T.solution1(a,b,arr1,arr2)){
+        for(int x : T.solution2(a,b,arr1,arr2)){
             System.out.print(x + " ");
         }
 
+    }
+
+
+    private ArrayList<Integer> solution2(int n, int m, int[] arr1 , int[] arr2){
+
+        //해결 알고리즘은 알았는데.. 1차원 배열의 정렬이 안됌
+        //1. 정렬
+        //2. 작은 수에 pointer ++
+        //3. 같으면 둘 다 ++
+        ArrayList<Integer> answer = new ArrayList<>();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int p1 =0 , p2 =0;
+        while (p1<n && p2<m){
+            if(arr1[p1]<arr2[p2]){
+                p1++;
+            }
+            else if (arr1[p1]>arr2[p2]) {
+                p2++;
+            }
+            else {
+                answer.add(arr1[p1]);
+                p1++;
+                p2++;
+            }
+        }
+
+
+        return answer;
     }
     /*
     설명
