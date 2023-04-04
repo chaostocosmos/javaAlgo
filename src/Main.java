39bbcf4b2ac253397543a30fbf7bb5d3b55a3ddf
chaostocosmos <kml1763@gmail.com>
2023-04-04 18:11:57 +0900

    Chapter3 최대 매출 찾기 문제 o(n) 방식풀이 및 4번 문제 추가

--- src/Main.java
+++ src/Main.java
@@ -19,10 +19,55 @@ public class Main {
         for(int x=0; x<n; x++){
             arr1[x] = sc.nextInt();
         }
+        System.out.println(T.solution3_a(n,k,arr1));
+
+    }
+    /*
+4. 연속 부분수열
+설명
+N개의 수로 이루어진 수열이 주어집니다.
+이 수열에서 연속부분수열의 합이 특정숫자 M이 되는 경우가 몇 번 있는지 구하는 프로그램을 작성하세요.
+만약 N=8, M=6이고 수열이 다음과 같다면
+
+1 2 1 3 1 1 1 2
+
+합이 6이 되는 연속부분수열은 {2, 1, 3}, {1, 3, 1, 1}, {3, 1, 1, 1}로 총 3가지입니다.
+
+입력
+첫째 줄에 N(1≤N≤100,000), M(1≤M≤100,000,000)이 주어진다.
+
+수열의 원소값은 1,000을 넘지 않는 자연수이다.
+
+8 6
+1 2 1 3 1 1 1 2
 
 
-        System.out.println(T.solution3(n,k,arr1));
+출력
+첫째 줄에 경우의 수를 출력한다.
 
+
+예시 입력 1
+     */
+
+    private int solution3_a(int n , int k ,int[] arr1){
+        // sum[first] + arr[i] - arr[i-k]
+
+        int answer = 0 , sum = 0;
+        // 처음 인자에 대한 합을 구한다음에
+        for(int i=0; i<k; i++) {
+            sum += arr1[i];
+        }
+        // 그 값을 잡아두고
+        answer = sum;
+
+
+        //밀면서 대소비교
+        for(int i=k; i<n; i++){
+            sum+=(arr1[i]-arr1[i-k]);
+            answer = Math.max(answer,sum);
+        }
+
+        return answer;
     }
 
     private int solution3(int n, int k, int[] arr1) {
@@ -31,16 +76,16 @@ public class Main {
         int maxnum = Integer.MIN_VALUE;
         int temp;
         while (p2<n){
-
             temp = 0;
+
             for(int i=0; i<k; i++){
-              temp += arr1[p1+i];
+                temp += arr1[p1+i];
             }
             if(temp>maxnum){
                 maxnum = temp;
                 answer = maxnum;
-            }
 
+            }
             p1++;
             p2++;
 
